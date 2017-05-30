36f8cf316c9b71ecb8d0ad78dc1a83d477d3e20f
Thinh Nguyen Viet <thinhnv@vega.com.vn>
2017-05-30 13:58:58 +0700

    abc

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -35,6 +35,9 @@ export default class Html extends Component {
 
           <link rel="shortcut icon" href="/favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </head>
+        <body>
+          <div id="content" dangerouslySetInnerHTML={{__html: content}}/>
           {/* styles (will be present only in production with webpack extract text plugin) */}
           {Object.keys(assets.styles).map((style, key) =>
             <link className="css-init" href={assets.styles[style]} key={key} media="screen, projection"
@@ -46,9 +49,6 @@ export default class Html extends Component {
           {/* can smoothen the initial style flash (flicker) on page load in development mode. */}
           {/* ideally one could also include here the style for the current page (Home.scss, About.scss, etc) */}
           { Object.keys(assets.styles).length === 0 ? <style dangerouslySetInnerHTML={{__html: require('../theme/bootstrap.config.js') + require('../containers/App/App.scss')._style}}/> : null }
-        </head>
-        <body>
-          <div id="content" dangerouslySetInnerHTML={{__html: content}}/>
           <script async dangerouslySetInnerHTML={{__html: `window.__data=${serialize(store.getState())};`}} charSet="UTF-8"/>
           <script async src={assets.javascript.main} charSet="UTF-8"/>
         </body>
