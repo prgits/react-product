614e3d1e26816778b4c05ab1da4c3c952f5a1c1a
Thinh Nguyen Viet <thinhnv@vega.com.vn>
2017-05-30 09:52:02 +0700

    iln

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,34 +4,19 @@ import Helmet from 'react-helmet';
 
 export default class Home extends Component {
   render() {
-    const styles = require('./Home.scss');
+    // const styles = require('./Home.scss');
+    require('./Home.css');
     return (
-      <div className={styles.home}>
+      <div className="home-page">
         <Helmet title="Home"/>
-        <div className={styles.masthead}>
-          <div className="container">
-            <h1>{config.app.title}</h1>
-
-            <h2>{config.app.description}</h2>
-
-            <p>
-              <a className={styles.github} href="https://github.com/erikras/react-redux-universal-hot-example"
-                 target="_blank">
-                <i className="fa fa-github"/> View on Github
-              </a>
-            </p>
-          </div>
-        </div>
-
         <div className="container">
-          <h3>From the author</h3>
-
+          <h1>{config.app.title}</h1>
+          <h2>{config.app.description}</h2>
           <p>
-            I cobbled this together from a wide variety of similar "starter" repositories. As I post this in June 2015,
-            all of these libraries are right at the bleeding edge of web development. They may fall out of fashion as
-            quickly as they have come into it, but I personally believe that this stack is the future of web development
-            and will survive for several years. I'm building my new projects like this, and I recommend that you do,
-            too.
+            <a href="https://github.com/erikras/react-redux-universal-hot-example"
+               target="_blank">
+              <i className="fa fa-github"/> View on Github
+            </a>
           </p>
         </div>
       </div>
